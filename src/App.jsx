9d5899c0650aeafaa42b6617940617d29ba84e5d
boomer934 <lupomarino09@gmail.com>
2025-08-20 20:26:26 +0200

    uncomment

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,11 @@ import './style.css'
 function App() {
   return (
     <>
-      {/* uncomment to use to do list
+    
      <div className='flex flex-col justify-center items-center'>
-     <h1 className='mb-3'>To Do List</h1>
-     <ToDoList/>
-     </div> */}
-     {/* other casual components */}
-     {/* ?<Counter/> */}
-     {/* <BgColor/> */}
+      <h1 className='mb-3'>To Do List</h1>
+      <ToDoList/>
+     </div>
     </>
     
   )
